fdb03cc0d49a5117392a45f49d862e6acde13c40
Desant <anturated@gmail.com>
2025-12-10 13:36:47 +0100

    fix: custom link

--- app/actions/encryptCustom.ts
+++ app/actions/encryptCustom.ts
@@ -4,5 +4,5 @@ import { CustomInfo, encryptPayload } from "@/lib/crypto";
 
 export async function encryptCustom(data: CustomInfo) {
   const token = encryptPayload(data);
-  return `http://localhost:3000/custom?t=${token}`;
+  return `https://anturated.dev/custom?t=${token}`;
 }
