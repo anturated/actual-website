803b483bdcd3bf1b6d08aae682a83b89d0d3087b
Desant <anturated@gmail.com>
2025-12-11 12:46:26 +0100

    add breadcrupmps

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,36 @@
+"use client";
+
 import Link from "next/link";
-import MaterialIcon from "./MaterialIcon";
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const parts = usePathname()
+    .split("/")
+    .filter(Boolean);
+
+  const crumps = parts.map((part, i) => ({
+    text: decodeURIComponent(part),
+    href: "/" + parts.slice(0, i + 1).join("/"),
+  }))
+
   return (
     <div className="flex flex-row justify-between items-center text-center text-sm h-[75px] w-full max-w-md mx-auto px-[8px] md:px-0">
-      <Link href="/" className="font-bold text-primary">
-        anturated
-      </Link>
+      <div className="flex flex-col md:flex-row items-start">
+        {/* homepage link */}
+        <Link href="/" className="font-bold text-primary">
+          anturated
+        </Link>
+        {/* breadcrumps */}
+        <div className="flex flex-row">
+          {
+            crumps.map(c => <span>
+              {"/"}
+              <Link href={c.href} className="text-secondary">{c.text}</Link>
+            </span>)
+          }
+        </div>
+      </div>
       <nav className="flex flex-row gap-[15px]">
         <HeaderLink href="/blog">
           posts
