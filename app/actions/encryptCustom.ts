94d1fcacc488ec40e03a7316da1afab5d2728f33
Desant <anturated@gmail.com>
2025-12-11 12:55:52 +0100

    add local/prod hostname for custom

--- app/actions/encryptCustom.ts
+++ app/actions/encryptCustom.ts
@@ -3,6 +3,8 @@
 import { CustomInfo, encryptPayload } from "@/lib/crypto";
 
 export async function encryptCustom(data: CustomInfo) {
+  const hostname = process.env.NODE_ENV === "production" ? "anturated.dev" : "localhost:3000";
+
   const token = encryptPayload(data);
-  return `https://anturated.dev/custom?t=${token}`;
+  return `https://${hostname}/custom?t=${token}`;
 }
