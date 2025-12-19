3b5e8ec5fafae3b8c34a93d24887402cd2516eb6
Desant <anturated@gmail.com>
2025-12-19 11:58:06 +0100

    chore: remove unnecessary deps

--- shell.nix
+++ shell.nix
@@ -4,15 +4,6 @@ pkgs.mkShell {
   name = "Functions Api";
 
   buildInputs = [
-    pkgs.dotnet-sdk_8
-    pkgs.dotnet-sdk_9
-    pkgs.dotnet-runtime_9
-    pkgs.dotnet-runtime_8
-    pkgs.omnisharp-roslyn
-
-    pkgs.azure-cli
-    pkgs.azure-functions-core-tools
-
     pkgs.fish
 
     pkgs.nodejs
